260e8e6dfb8f9ba548bd27b75ceac376e22f398a
MD Abu Dayan Siddik <dayansiddik94@gmail.com>
2020-03-25 01:36:41 +0600

    Add files via upload

---
+++ uva 10935.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    unsigned char n = 0, i = 0, left = 0;
+    stack<int> cards;
+    while(scanf("%hhu", &n) == 1)
+    {
+        left = n;
+        throw_card = 0;
+        while(left != n)
+        {
+            for(i = 1; i <= left; i++)
+            {
+
+            }
+        }
+    }
+    return 0;
+}
